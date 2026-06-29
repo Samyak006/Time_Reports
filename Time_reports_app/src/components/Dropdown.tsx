b4c672dc4da63988a20bc4cf53c640fd3cc7892b
Samyak006 <samyak.rokade.o@gmail.com>
2026-06-28 23:11:47 -0500

    implemented useRef logic for dropdown

--- Time_reports_app/src/components/Dropdown.tsx
+++ Time_reports_app/src/components/Dropdown.tsx
@@ -1,44 +1,66 @@
 import type React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export interface IDropdownProps {
-    options: string[];
-    setFilter: React.Dispatch<React.SetStateAction<string>>;
+  options: string[];
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Dropdown ({options, setFilter}: IDropdownProps) {
+export default function Dropdown({ options, setFilter }: IDropdownProps) {
   const [dropdownList, setDropdownList] = useState<string[]>([...options]);
-  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false)
-  const [inputValue, setInputValue] = useState<string>('')
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const onChangeHandler =(e:React.ChangeEvent<HTMLInputElement>):void => {
-    e.preventDefault()
-    setInputValue(e.target.value)
-    setDropdownList(options.filter(str => str.includes(e.target.value)))
-  }
-  
-  const onClickBtnHandler=(e:React.MouseEvent<HTMLButtonElement>):void =>{
-    e.preventDefault()
-    setFilter(e.currentTarget.value)
-    setInputValue(e.currentTarget.value)
-    setIsDropdownVisible(false)
-  }
-  
-  document.addEventListener("mousedown", (e)=>{
-    const target = e.target as HTMLInputElement | HTMLButtonElement;
-    if (target.parentNode && (target.parentNode as HTMLElement).className.includes('dropdown') === false){
-      setIsDropdownVisible(false)
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+    setDropdownList(options.filter((str) => str.includes(e.target.value)));
+  };
+
+  const onClickBtnHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    setFilter(e.currentTarget.value);
+    setInputValue(e.currentTarget.value);
+    setIsDropdownVisible(false);
+  };
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsDropdownVisible(false);
+      }
     }
-  });
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <>
-        <div className="dropdown flex flex-col">
-          <input className="border rounded" placeholder="Filter values" onClick={()=> setIsDropdownVisible(true)} onChange={(e)=>onChangeHandler(e)} value={inputValue}/>
-            {isDropdownVisible && dropdownList.map((option, index)=>(          
-              <button className="border rounded" key={index} value={option} onClick={(e)=>onClickBtnHandler(e)}>{option}</button>
-            ))}
-        </div>
+      <div
+        ref={dropdownRef} 
+        className="dropdown flex flex-col">
+        <input
+          className="border rounded"
+          placeholder="Filter values"
+          onClick={() => setIsDropdownVisible(true)}
+          onChange={(e) => onChangeHandler(e)}
+          value={inputValue}
+        />
+        {isDropdownVisible &&
+          dropdownList.map((option, index) => (
+            <button
+              className="border rounded"
+              key={index}
+              value={option}
+              onClick={(e) => onClickBtnHandler(e)}
+            >
+              {option}
+            </button>
+          ))}
+      </div>
     </>
   );
 }
