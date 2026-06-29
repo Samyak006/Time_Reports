8c04e6270864665473f469a2fcfd1bd28c3f2129
Samyak006 <samyak.rokade.o@gmail.com>
2026-06-28 22:36:23 -0500

    added outside click function.

--- Time_reports_app/src/components/Dropdown.tsx
+++ Time_reports_app/src/components/Dropdown.tsx
@@ -10,21 +10,31 @@ export default function Dropdown ({options, setFilter}: IDropdownProps) {
   const [dropdownList, setDropdownList] = useState<string[]>([...options]);
   const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false)
   const [inputValue, setInputValue] = useState<string>('')
+
   const onChangeHandler =(e:React.ChangeEvent<HTMLInputElement>):void => {
     e.preventDefault()
     setInputValue(e.target.value)
     setDropdownList(options.filter(str => str.includes(e.target.value)))
   }
+  
   const onClickBtnHandler=(e:React.MouseEvent<HTMLButtonElement>):void =>{
     e.preventDefault()
     setFilter(e.currentTarget.value)
     setInputValue(e.currentTarget.value)
     setIsDropdownVisible(false)
   }
+  
+  document.addEventListener("mousedown", (e)=>{
+    const target = e.target as HTMLInputElement | HTMLButtonElement;
+    if (target.parentNode && (target.parentNode as HTMLElement).className.includes('dropdown') === false){
+      setIsDropdownVisible(false)
+    }
+  });
+
   return (
     <>
-        <div className=" flex flex-col">
-          <input className=" border rounded" placeholder="Filter values" onClick={()=> setIsDropdownVisible(prev=>!prev)} onChange={(e)=>onChangeHandler(e)} value={inputValue}/>
+        <div className="dropdown flex flex-col">
+          <input className="border rounded" placeholder="Filter values" onClick={()=> setIsDropdownVisible(true)} onChange={(e)=>onChangeHandler(e)} value={inputValue}/>
             {isDropdownVisible && dropdownList.map((option, index)=>(          
               <button className="border rounded" key={index} value={option} onClick={(e)=>onClickBtnHandler(e)}>{option}</button>
             ))}
