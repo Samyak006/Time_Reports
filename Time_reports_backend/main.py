f615b05713abc4ca3be73719c82e101b5b85946b
Samyak006 <samyak.rokade.o@gmail.com>
2026-07-01 00:05:09 -0500

    connected the backend to sandbox

--- Time_reports_backend/main.py
+++ Time_reports_backend/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI #type: ignore
-
+from fastapi import FastAPI, Depends 
+from .services.connection import get_oauth2_token
+from .services.timekeepers import get_timekeepers
+from .services.clients import get_clients
+from .services.matters import get_matters
+from authlib.integrations.httpx_client import AsyncOAuth2Client #type: ignore
+from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
 async def root():
 	return {"message":"Hello World"}
+
+@app.get("/timekeepers")
+async def timekeepers(client: Annotated[AsyncOAuth2Client, Depends(get_oauth2_token)]):
+	return await get_timekeepers(client) 
+
+@app.get("/clients")
+async def matters(client: Annotated[AsyncOAuth2Client, Depends(get_oauth2_token)]):
+	return await get_matters(client) 
+
+@app.get("/matters")
+async def clients(client: Annotated[AsyncOAuth2Client, Depends(get_oauth2_token)]):
+	return await get_clients(client) 
--- Time_reports_backend/services/connection.py
+++ Time_reports_backend/services/connection.py
@@ -1,17 +1,31 @@
 from authlib.integrations.httpx_client import AsyncOAuth2Client #type: ignore
-from ..config import settings
+from ..config import Settings, settings
+
+async def get_oauth2_token():
+    client = AsyncOAuth2Client(
+        client_id=settings.client_id
+        ,client_secret=settings.client_secret
+        ,token_endpoint=settings.access_token_url
+    )
+    await client.fetch_token(grant_type="client_credentials")
+    try:
+        yield client
+    finally:
+        await client.aclose()
 
 
-class Connection:
-    def __init__(self):    
-        self.token_url = settings.access_token_url
-        self.client_id = settings.client_id
-        self.client_secret = settings.client_secret
+# class Connection:
+#     def __init__(self, settings:Settings):    
+#         self.token_url = settings.access_token_url
+#         self.client_id = settings.client_id
+#         self.client_secret = settings.client_secret
 
-    async def authorize(self):
-        # Initialize Authlib's custom async client wrapper
-        async with AsyncOAuth2Client(self.client_id, self.client_secret) as client:
-            # Fetch and manage the token internally
-            await client.fetch_token(self.token_url, grant_type="client_credentials")
-            return client
-        
+#     async def init_client(self):
+#         if not self.client:
+#             async with AsyncOAuth2Client(
+#                 client_id=settings.client_id
+#                 ,client_secret=settings.client_secret
+#                 ,token_endpoint=settings.access_token_url
+#         )    as client:
+#                 token = await client.fetch_token(grant_type="client_credentials")
+#                 yield token
